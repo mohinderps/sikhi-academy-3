f50330d5e391aab02d6bd66af23accaeb7be48c0
mohinderps <mohin19derp@gmail.com>
2024-10-18 22:50:17 +0530

    Fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,10 +7,25 @@ import miscRoutes from "./routes/misc.routes";
 
 const app = express();
 
+const isProduction = process.env.NODE_ENV === "production";
+const allowedOrigins = isProduction
+  ? ["https://sikhiacademy.com"]
+  : ["http://localhost:8080"];
+
 app.use(
   cors({
-    origin: "https://sikhiacademy.com",
     optionsSuccessStatus: 200,
+    origin: (origin, callback) => {
+      if (!origin) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        return callback(null, true);
+      }
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
 
